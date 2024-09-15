11f2254c6333a2b216d97ab454af05750777f676
Dmitry Chernenkov <dmitrychee@gmail.com>
2024-09-15 21:21:17 +0300

    add colors and sizes

--- src/theme/_config.ts
+++ src/theme/_config.ts
@@ -3,32 +3,55 @@ import { DarkTheme, DefaultTheme } from "@react-navigation/native";
 import type { ThemeConfiguration } from "@/types/theme/config";
 
 const colorsLight = {
-  red500: "#C13333",
+  grayTextTitle: "#1B1721",
+  grayTextSubtitle: "#54536A",
+  grayTextBody: "#211C28",
+  grayTextCaption: "#72708F",
+  white: "#FFFFFF",
+  green500: "#15BC76",
+  apricot500: "#DE7D54",
+  orange500: "#F23832",
+  whiteOpacity70: "#FFFFFFB3",
+  graySuva: "#868686",
+  red500: "#BD0233",
+  purple500: "#807E9A",
+
   gray800: "#303030",
   gray400: "#4D4D4D",
   gray200: "#A1A1A1",
   gray100: "#DFDFDF",
   gray50: "#EFEFEF",
-  purple500: "#44427D",
   purple100: "#E1E1EF",
   purple50: "#1B1A23",
   statusBar: "#FFFFFF",
 } as const;
 
 const colorsDark = {
+  grayTextTitle: "#1B1721",
+  grayTextSubtitle: "#54536A",
+  grayTextBody: "#211C28",
+  grayTextCaption: "#72708F",
+  white: "#FFFFFF",
+  green500: "#15BC76",
+  apricot500: "#DE7D54",
+  orange500: "#F23832",
+  whiteOpacity70: "#FFFFFFB3",
+  graySuva: "#868686",
+
   red500: "#C13333",
+  purple500: "#A6A4F0",
+
   gray800: "#E0E0E0",
   gray400: "#969696",
   gray200: "#BABABA",
   gray100: "#000000",
   gray50: "#EFEFEF",
-  purple500: "#A6A4F0",
   purple100: "#252732",
   purple50: "#1B1A23",
   statusBar: "#FFFFFF",
 } as const;
 
-const sizes = [12, 16, 24, 32, 40, 80] as const;
+const sizes = [12, 14, 16, 24, 32, 40, 80] as const;
 
 export const config = {
   colors: colorsLight,
